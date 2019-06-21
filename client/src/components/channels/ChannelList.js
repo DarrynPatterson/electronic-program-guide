ff2335d5d79635757224d41b39c39c568fac6937
Darryn <darryn.patterson@gmail.com>
2019-06-21 11:10:14 +0200

    Display channel list.

--- client/src/components/channels/ChannelList.js
+++ client/src/components/channels/ChannelList.js
@@ -4,6 +4,8 @@ import { getChannels } from "../../actions/channelActions";
 import PropTypes from "prop-types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import ChannelListItem from "./ChannelListItem";
+import moment from "moment";
 
 class ChannelList extends Component {
 
@@ -23,6 +25,55 @@ class ChannelList extends Component {
     return (
       <>
         {isLoading && (<FontAwesomeIcon icon={faSpinner} spin />)}
+
+        <div className="list-group">
+          {!isLoading && channels.map(channelData => {
+
+              const orderedPrograms = channelData.schedules.sort((a, b) => a.start > b.start);
+              const nextProgram = orderedPrograms[0];
+              const startTime = moment(nextProgram.start).format("HH:mm");
+              const endTime = moment(nextProgram.end).format("HH:mm");
+
+              // Define channel object
+              let channel = {
+                id: channelData.id,
+                title: channelData.title,
+                logo: channelData.images.logo
+              };
+
+              // Define program object
+              let program = {
+                title: nextProgram.title,
+                time: `${startTime} - ${endTime}`
+              };
+
+              // Define progress object
+              let progress = {
+                isProgressAvailable: false,
+                isFavorite: null,
+                percentageComplete: null
+              };
+
+              // Does this program have user preferences?
+              const preferences = userPreferences.programs.find((program) => {
+                return program.id === nextProgram.id
+              });
+
+              // Update progress object if preferences exist              
+              if (typeof preferences !== "undefined") {
+                progress.isProgressAvailable = true;
+                progress.isFavorite = preferences.progress.position > 0;
+                progress.percentageComplete = preferences.progress.percentage;
+              }
+
+              return (
+                <ChannelListItem
+                  channel={channel}
+                  program={program}
+                  progress={progress} />
+              );
+            })}
+        </div>
       </>
     );
   }
---
+++ client/src/components/channels/ChannelListItem.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+
+class ChannelListItem extends Component {
+  render() {
+    const channel = this.props.channel;
+    const program = this.props.program;
+    const progress = this.props.progress;
+
+    const preferenceContent = (
+      <div className="progress">
+        <div className="progress-bar progress-bar-striped bg-warning" role="progressbar" style={{ width: progress.percentageComplete + "%" }} aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+      </div>)
+
+    const favoriteIconColor = progress.isFavorite ? "orange" : "";
+
+    return (
+      <a id={channel.id} href="#" className="list-group-item list-group-item-action flex-column align-items-start">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-2 text-center">
+              <img style={{ width: "100px", height: "100px" }} src={channel.logo} alt={channel.title} />
+              <h5 className="mt-2">{channel.title}</h5>
+            </div>
+            <div className="col-sm-8">
+              <h3>{program.title}</h3>
+              <div className="mt-4 mb-4">
+                <h5 className="text-muted">{program.time}</h5>
+              </div>
+              {progress.isProgressAvailable && preferenceContent}
+            </div>
+            <div className="col-sm-2">
+              <div className="float-right">
+                <FontAwesomeIcon icon={faStar} size="2x" style={{ color: favoriteIconColor }} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </a>
+    );
+  }
+}
+
+export default ChannelListItem;
